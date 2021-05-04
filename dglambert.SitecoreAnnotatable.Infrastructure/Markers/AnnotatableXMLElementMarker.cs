1e3b67fba7a38f1e7e78f749806245ad96227c79
Devin Lambert <dlambert@oattravel.com>
2021-05-03 21:23:22 -0400

    Adding required sitecore nuget packages, and creating Marker code & tests

---
+++ dglambert.SitecoreAnnotatable.Infrastructure/Markers/AnnotatableXMLElementMarker.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Mvc.ExperienceEditor.Presentation;
+using System;
+
+namespace dglambert.SitecoreAnnotatable.Infrastructure.Markers
+{
+    public class AnnotatableXMLElementMarker : IMarker
+    {
+        public AnnotatableXMLElementMarker(string renderingName, string dataSource)
+        {
+
+        }
+
+        public string GetStart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetEnd()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dglambert.SitecoreAnnotatable.Tests/Markers/AnnotatableXMLElementMarkerTests.cs
@@ -0,0 +1,26 @@
+﻿using dglambert.SitecoreAnnotatable.Infrastructure.Markers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sitecore.Mvc.ExperienceEditor.Presentation;
+using System;
+
+namespace dglambert.SitecoreAnnotatable.Tests.Markers
+{
+    [TestClass]
+    public class AnnotatableXMLElementMarkerTests
+    {
+        [TestMethod]
+        public void CTOR_Valid()
+        {
+            //Arrange
+            string providedRenderingName = "";
+            string providedDataSource = null;
+
+            //Act
+            IMarker actualMarker = new AnnotatableXMLElementMarker(providedRenderingName, providedDataSource);
+
+            //Assert
+            Assert.IsNotNull(actualMarker);
+
+        }
+    }
+}
