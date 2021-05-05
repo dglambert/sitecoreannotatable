8b658663a293573bba754fac5723d57e96d526c8
Devin Lambert <dlambert@oattravel.com>
2021-05-04 23:47:34 -0400

    Implement EndAllWrappers pipeline and tests

---
+++ dglambert.SitecoreAnnotatable.Infrastructure/Pipelines/EndAllRenderingWrappers.cs
@@ -0,0 +1,20 @@
+﻿using dglambert.SitecoreAnnotatable.Infrastructure.Wrappers;
+using Sitecore.Mvc.ExperienceEditor.Presentation;
+using Sitecore.Mvc.Pipelines.Response.RenderRendering;
+using System;
+using System.Linq;
+
+
+namespace dglambert.SitecoreAnnotatable.Infrastructure.Pipelines
+{
+    public class EndAllRenderingWrappers : RenderRenderingProcessor
+    {
+        public override void Process(RenderRenderingArgs args)
+        {
+            foreach(IDisposable wrapper in args.Disposables.OfType<IDisposable>())
+            {
+                wrapper.Dispose();
+            }
+        }
+    }
+}
---
+++ dglambert.SitecoreAnnotatable.Tests/Pipelines/EndAllRenderingWrappersTests.cs
@@ -0,0 +1,43 @@
+﻿using dglambert.SitecoreAnnotatable.Infrastructure.Pipelines;
+using dglambert.SitecoreAnnotatable.Infrastructure.Wrappers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Sitecore.Mvc.ExperienceEditor.Presentation;
+using Sitecore.Mvc.Pipelines.Response.RenderRendering;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace dglambert.SitecoreAnnotatable.Tests.Pipelines
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class EndAllRenderingWrappersTests
+    {
+        [TestMethod]
+        public void Process_Valid_Args()
+        {
+            //Arrange
+            Rendering providedRendering = new Mock<Rendering>().Object;
+            TextWriter providedTextWriter = new Mock<TextWriter>().Object;
+            RenderRenderingArgs providedRenderRenderingArgs = new RenderRenderingArgs(providedRendering, providedTextWriter);
+
+            Mock<IDisposable> mockAnnotatableRenderingWrapper = new Mock<IDisposable>();
+            providedRenderRenderingArgs.Disposables.Add(mockAnnotatableRenderingWrapper.Object);
+
+            EndAllRenderingWrappers endAllRenderingWrappers = new EndAllRenderingWrappers();
+
+
+            //Act
+            endAllRenderingWrappers.Process(providedRenderRenderingArgs);
+
+            //Assert
+            mockAnnotatableRenderingWrapper.Verify(i => i.Dispose(), Times.Once);
+
+
+        }
+
+        
+    }
+}
