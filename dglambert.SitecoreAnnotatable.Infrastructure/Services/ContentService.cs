5ddfeb6de65db87a60a5d73b4244fccfd44371ff
Devin Lambert <dlambert@oattravel.com>
2021-05-09 22:04:18 -0400

    Intiall Implementaiton of ContentService, CTOR only;

---
+++ dglambert.SitecoreAnnotatable.Infrastructure/Services/ContentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Sitecore.Data;
+
+namespace dglambert.SitecoreAnnotatable.Infrastructure.Services
+{
+    public class ContentService : IContentService
+    {
+        Database _database;
+        public ContentService(Database database)
+        {
+            _database = database ?? throw new ArgumentNullException("database must be a non-null value.");
+        }
+    }
+}
---
+++ dglambert.SitecoreAnnotatable.Tests/Services/ContentServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Sitecore.Data;
+using dglambert.SitecoreAnnotatable.Infrastructure.Services;
+
+namespace dglambert.SitecoreAnnotatable.Tests.Services
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class ContentServiceTests
+    {
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void CTOR_NullDatabase_ThrowsException()
+        {
+            //Arrange
+            Database providedDatabase = null;
+            
+            //Act
+            IContentService actualContentService = new ContentService(providedDatabase);
+
+            //Assert
+            Assert.IsNotNull(actualContentService);
+        }
+    }
+}
